4c40547a9b525b8b0e06c9384e53e372ce8ec77a
pratapshingane18 <sushrutrao465@gmail.com>
2023-01-09 13:14:05 +0530

    Completed

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const navbarEl = document.querySelector(".navbar");
 
-console.log(navbarEl);
+// console.log(navbarEl);
+
+const bottomContainerEl = document.querySelector(".bottom-container");
+
+// console.log(navbarEl.offsetHeight);
+// console.log(bottomContainerEl.offsetTop);
+
+window.addEventListener("scroll",() =>{
+    if(window.scrollY > bottomContainerEl.offsetTop - navbarEl.offsetHeight - 50){
+        navbarEl.classList.add("active");
+
+    }
+    else{
+        navbarEl.classList.remove("active");
+
+    }
+});
